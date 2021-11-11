0b3be137c03676a84d73bcfbd28203e7bc7db520
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2021-11-10 21:50:27 -0600

    Update ExpressionTree.java

--- src/main/java/com/mycompany/datos1proyecto2/ExpressionTree.java
+++ src/main/java/com/mycompany/datos1proyecto2/ExpressionTree.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *@author Valerine
  */
 package com.mycompany.datos1proyecto2;
 import java.util.Stack;
